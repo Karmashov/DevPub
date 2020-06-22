7f0746dbda522016d9ebd5a0917de77efecf7ac0
Andrey <a.v.karmashov@gmail.com>
2020-06-22 09:35:43 +0300

    Liquibase experiments

--- src/main/java/com/skillbox/devpub/main/model/User.java
+++ src/main/java/com/skillbox/devpub/main/model/User.java
@@ -39,7 +39,7 @@ public class User {
     private String code;
 
     @Type(type = "text")
-    @Column(nullable = false)
+//    @Column(nullable = false)
     private String photo;
 
     @OneToMany(mappedBy = "moderator")
